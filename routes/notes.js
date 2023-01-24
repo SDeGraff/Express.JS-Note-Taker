5e0face6822e6d4ab2d53848ccd532ddb828fc04
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2023-01-24 14:51:04 -0600

    repair index.js

--- routes/notes.js
+++ routes/notes.js
@@ -4,12 +4,12 @@ const notes = require('express').Router()
 const {v4: uuidv4} = require('uuid')
 // getting uuid
 
-const {readAndAppend, readFromFile} = require('../note/fsFunction')
+const {readAndAppend, readFromFile} = require('../lib/fsFunct')
 // getting readandappedn and readfromfile 
 
 const fs = require('fs')
-const { response } = require('express')
-const { request } = require('http')
+// const { response } = require('express')
+// const { request } = require('http')
 
 notes.get('/', (request, response) =>
     readFromFile('./db/db.json').then((data) => response.json(JSON.parse(data)))
@@ -26,13 +26,13 @@ notes.post('/', (request,response) => {
         const info = readAndAppend(newNote, './db/db.json')
         response.json(info)
     } else {
-        response.json('error in posting note')
+        response.json('Note Posting Error')
     }
 })
 
-notes.delete('/', (request, response) => {
+notes.delete('/:id', (request, response) => {
     const id = request.params.id
-    fs.readFile('./db/db.json', 'utf-8', (error, data) => {
+    fs.readFile('./db/db.json', 'utf8', (error, data) => {
         const dbdata = JSON.parse(data)
         const filteredDBData = dbData.filter(note => note.id !== id)
         fs.writeFile('./db/db.json', JSON.stringify(filteredDBData), (error) => {
