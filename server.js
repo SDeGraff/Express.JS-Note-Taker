af2083966211cad697809e9848081add3f6a981f
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2023-01-27 10:55:15 -0600

    fixed port?

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
 // importing express from node modules
 
-const express = require('express')
-const { response } = require('express')
-const { request } = require('http')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 //imports path
-const path = require('path')
+const path = require('path');
 
-const api = require('./routes')
+const api = require('./routes');
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001;
 
 //middleware
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use('/api', api)
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', api);
+app.use(express.static('public'));
 
 app.get('/', (request, response) =>
     response.sendFile(path.join(__dirname, '/public/index.html'))    
-)
+);
 
 app.get('/notes', (request, response) =>
     response.sendFile(path.join(__dirname, '/public/notes.html'))
-)
+);
 
 app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${3001}`)
-)
+console.log(`server is ready on port ${PORT}`)
+);
 
 
 
